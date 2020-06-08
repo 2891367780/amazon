edf7739cd0e82a24decd9fcabe1a84cb1386262e
huangrun <2891367780@qq.com>
2020-06-08 22:17:40 +0800

    6.8晚上提交

---
+++ php/4.list.php
@@ -0,0 +1,48 @@
+  <?php
+
+    //设置跨域的header
+    header('Access-Control-Allow-Origin:*');  //允许跨域的网址，*所有的网址。
+    header('Access-Control-Allow-Method:POST,GET'); //允许跨域的请求方式。
+
+    //php引入公共文件
+    require "conn.php";
+
+    $pagesize = 8; //单个页面展示的数据条数
+
+    $sql = "select * from amazonindex"; //获取所有的数据
+    $result = $conn->query($sql); //获取数据的结果集(记录集)
+
+    $num = $result->num_rows; //记录集的总条数  22
+
+    $pagenum = ceil($num / $pagesize); //获取页数  3
+
+
+    //获取前端的传来的页面，根据页码查询对应的数据，返回给前端。
+    if (isset($_GET['page'])) {
+        $pagevalue = $_GET['page'];
+    } else {
+        $pagevalue = 1;
+    }
+
+    $page = ($pagevalue - 1) * $pagesize;
+
+    //limit
+    //limit接收一个或者两个数字参数(整数)
+    //参1：数据开始位置的索引(从0开始)，偏移量
+    //参2：返回的记录集数目。
+    //limit 0,10  从偏移量0开始 取10条
+    //limit 10,10  从偏移量5开始 取10条
+    //limit 20,10 从偏移量14开始 取10条
+
+    $sql1 = "select * from amazonindex limit $page,$pagesize";
+    $res = $conn->query($sql1);
+
+
+    //通过二维数组输出
+    // $result->num_rows; //记录集的条数
+    // $result->fetch_assoc(); //逐条获取记录集的值，结果是数组。
+    $arr = array();
+    for ($i = 0; $i < $res->num_rows; $i++) {
+        $arr[$i] = $res->fetch_assoc();
+    }
+    echo json_encode($arr);//输出接口
--- src/script/1.index.js
+++ src/script/1.index.js
@@ -1,22 +1,22 @@
-!function($){
-        const $mainleft2_list=$('.render1')
-        const $mainleft3_list=$('.mainleft3-list')
-        $.ajax({
-            url:'http://localhost/amazon/php/1.index.php',
-            dataType: 'json'
-        }).done(function(data){
-            // console.log(data);
-            // 渲染第一部分
-            let $main1str=''
-            $.each(data,function(index,value){
-                // console.log(value);
-                
-                $main1str+=`
+!(function ($) {
+  const $mainleft2_list = $(".render1");
+  const $mainleft3_list = $(".mainleft3-list");
+  //1.请求数据渲染----------------------------------------------------
+  $.ajax({
+    url: "http://localhost/amazon/php/1.index.php",
+    dataType: "json",
+  }).done(function (data) {
+    // console.log(data);
+    // 渲染第一部分--------------------------------------------
+    let $main1str = "";
+    $.each(data, function (index, value) {
+      // console.log(value);
+      if (index < 10) {
+        $main1str += `
                 <li>
                   <a href="" target="_blank">
                     <img
-                      src="${value.url}"
-                      alt=""
+                    class="lazy" data-original="${value.url}" width="200" height="200"
                     />
                     <span class="price">
                       <em>￥</em>
@@ -26,24 +26,22 @@
                     <i>距结束</i>
                   </a>
                 </li>
-                `
+                `;
+      }
+      $mainleft2_list.html($main1str);
+    });
 
-                $mainleft2_list.html($main1str)
-            })
-
-            // 渲染第二第三部分
-            let $main2str=''
-            $.each(data,function(index,value){
-                // console.log(value);
-                if(index<5){
-
-               
-                $main2str+=`
+    // 渲染第二第三部分---------------------------------------
+    // 懒加载中img必须添加class="lazy" data-original="${value.url}" width="200" height="200"
+    let $main2str = "";
+    $.each(data, function (index, value) {
+      // console.log(value);
+      if (index < 5) {
+        $main2str += `
                 <li>
                   <a href="" target="_blank">
                     <img
-                      src="${value.url}"
-                      alt=""
+                    class="lazy" data-original="${value.url}" width="200" height="200"
                     />
                     <span class="price">
                       <em>￥</em>
@@ -52,12 +50,73 @@
                     
                   </a>
                 </li>
-                `
-                
-            }
-                $mainleft3_list.html($main2str)
-            })
+                `;
+      }
+    });
+    $mainleft3_list.html($main2str);
 
-            
-        })
-}(jQuery)
+    //2.添加懒加载效果------------------------------------------
+    $(function () {
+      $("img.lazy").lazyload({ effect: "fadeIn" });
+    });
+  });
+
+  //3.二级导航
+  //   const $menuli = $(".menu ul li");
+  //   const $menulist = $(".menulist");
+  //   const $menuitem = $(".menulist .item");
+  //   const $menu = $(".menu");
+
+  //   $menuli.on("mouseover", function () {
+  //     $menulist.show();
+  //     $menuitem.eq($(this).index()).show().siblings(".item").hide();
+  //   });
+
+  //   $menuli.on("mouseout", function () {
+  //     $menulist.hide();
+  //   });
+
+  //   //cartlist自身移入移出
+  //   $menulist.on("mouseover", function () {
+  //     $(this).show();
+  //   });
+
+  //   $menulist.on("mouseout", function () {
+  //     $(this).hide();
+  //   });
+
+  const $menuli = $(".menu li");
+  const $cartlist = $(".menulist");
+  const $items = $(".item");
+  const $banner = $(".banner");
+
+  //1.左侧的菜单li进行移入移出。
+  $menuli.on("mouseover", function () {
+    // $(this).addClass("active").siblings(".menu li").removeClass("active");
+    //滚动条的top值如果大于$banner盒子的top值，$cartlist的top就是前面滚动条的top值 - $banner盒子的top值
+    // if ($(window).scrollTop() > $banner.offset().top) {
+    //   $cartlist.css({
+    //     top: $(window).scrollTop() - $banner.offset().top,
+    //   });
+    // } else {
+    //   $cartlist.css({
+    //     top: 0,
+    //   });
+    // }
+    $cartlist.show();
+    $items.eq($(this).index()).show().siblings(".item").hide();
+  });
+
+  $menuli.on("mouseout", function () {
+    $cartlist.hide();
+  });
+
+  //cartlist自身移入移出
+  $cartlist.on("mouseover", function () {
+    $(this).show();
+  });
+
+  $cartlist.on("mouseout", function () {
+    $(this).hide();
+  });
+})(jQuery);
